2a00b326169c338029e15461d4070611fc30aa48
Brightini <brightiniabasi@gmail.com>
2023-04-25 17:37:28 +0100

    Added if condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,23 +6,27 @@
  *
  * @array: an array of integers
  * @size: size of array @array
-*/
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	/* first loop to iterate through the entire array */
-	for (i = 0; i < size - 1; i++)
+
+	if (array)
 	{
-		/* second loop for swapping */
-		for (j = 0; j < size - 1 - i; j++)
+		/* first loop to iterate through the entire array */
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			/* second loop for swapping */
+			for (j = 0; j < size - 1 - i; j++)
 			{
-				temp = array[j]; /* stores value temporary for swap */
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size); /* prints array for every swap */
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j]; /* stores value temporary for swap */
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					print_array(array, size); /* prints array for every swap */
+				}
 			}
 		}
 	}
