c47f000999b7b6067d8b3b8bfa4e6b4983396dc0
Brightini <brightiniabasi@gmail.com>
2023-04-27 23:44:24 +0100

    Reviewed code :ok_hand:

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,80 @@
 #include "sort.h"
+
+/**
+ * swap - swaps the values at two different indices of an array
+ * @first: value at first index
+ * @second: value at second index
+*/
+void swap(int *first, int *second)
+{
+	int temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
+}
+
+/* Auxiliary function for the quick_sort function */
+void _qsort(int *array, int low, int high, int size);
+
 /**
  * quick_sort - function that sorts an array
- * of integers in ascending order using the
- * quick sort algorithm
+ *				of integers in ascending order using the
+ *				quick sort algorithm.
+ *				It implements the Lomuto partition scheme.
  *
  * @array: input arrray
  * @size: size of the array
- * Return: no return
  */
 void quick_sort(int *array, size_t size)
 {
+	/* Call the auxiliary function to sort the array */
 	_qsort(array, 0, size - 1, size);
 }
+
 /**
  * _qsort - auxiliar function for the
- * quick_sort function
- * @a: input arrray
+ *			quick_sort function
+ *
+ * @array: input arrray
  * @low: index for the first element
  * @high: index for the last element
  * @size: size of the array
- * Return: no return
  */
-void _qsort(int *a, int low, int high, int size)
+void _qsort(int *array, int low, int high, int size)
 {
-	int p, w, i;
-	int tmp;
+	int pivot, wall, i;
 
+	/* If there are at least two elements to sort */
 	if (low < high)
 	{
-		p = high;
-		w = low;
+		/* Choose the last element as the pivot */
+		pivot = high;
+		wall = low;
+		/* Partition the array into two subarrays */
 		for (i = low; i < high; i++)
 		{
-			if (a[i] < a[p])
+			if (array[i] < array[pivot])
 			{
-				if (i != w)
+				/* Swap the current element with the first */
+				/* element to the right of the wall */
+				if (i != wall)
 				{
-					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
-					print_array(a, size);
+					swap(&array[i], &array[wall]);
+					print_array(array, size);
 				}
-				w++;
+				/* Move the wall one position to the right */
+				wall++;
 			}
 		}
-		if (w != p && a[w] != a[p])
+		/* Swap the pivot with the element at the wall position */
+		if (wall != pivot && array[wall] != array[pivot])
 		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
-			print_array(a, size);
+			swap(&array[wall], &array[pivot]);
+			print_array(array, size);
 		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
+		/* Sort the subarrays to the left and right of the pivot recursively */
+		_qsort(array, low, wall - 1, size);
+		_qsort(array, wall + 1, high, size);
 	}
 }
