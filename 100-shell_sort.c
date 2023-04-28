12bba8827acd3ed30587659ac55eb96cd0a722cd
Brightini <brightiniabasi@gmail.com>
2023-04-29 00:29:47 +0100

    Removed identical_array function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,28 +1,6 @@
 #include "sort.h"
 
 /**
- * identical_element - checks if @array is an array of identical elements
- *
- * @array: input array of integers
- * @size: size of @array
- *
- * Return: 0 if @array is an array of identical elements. Otherwise 1
-*/
-int identical_element(int *array, size_t size)
-{
-	int element;
-	size_t i;
-
-	element = array[0];
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] != element)
-			return (1);
-	}
-	return (0);
-}
-
-/**
  * swap - swaps the values at two different indices of an array
  *
  * @first: value at first index
@@ -47,10 +25,10 @@ void swap(int *first, int *second)
 */
 void shell_sort(int *array, size_t size)
 {
-	int i, identical = 1;
+	int i;
 	size_t j, interval, gap;
 
-	if (!array || identical == 0)
+	if (!array)
 		return;
 
 	/* generate Knuth sequence */
@@ -66,7 +44,7 @@ void shell_sort(int *array, size_t size)
 		{
 			for (i = j - gap; i >= 0; i = i - gap)
 			{
-				if (array[i + gap] > array[i])
+				if (array[i + gap] < array[i])
 					break;
 				swap(&array[i + gap], &array[i]);
 			}
